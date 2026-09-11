f75818c6d86bbd03fefcb69f398f199e51324d4e
2605736 <2605736@sit.singaporetech.edu.sg>
2026-09-12 06:54:49 +0800

    accept stock values as integers

--- Week2_Lab/auditor.py
+++ Week2_Lab/auditor.py
@@ -1,18 +1,21 @@
 def main ():
-    # initialize variables
+    # 1. initialize variables
     total_inventory = 0
     failed_entries = 0
 
     print("--- Smart Inventory Auditor ---")
     print("Type 'quit' to exit:\n")
 
-    # continuous loop
+    # 2. continuous loop
     while True:
-        usr_input = input("Enter stock quantity (tupe): ")
+        usr_input = input("Enter stock quantity: ")
 
         # check for exit command 
         if usr_input.lower() == "quit":
             break
+        
+        # 3. accept stock values as integers
+        stock_quantity = int(usr_input)
 
 if __name__ == "__main__":
     main()
