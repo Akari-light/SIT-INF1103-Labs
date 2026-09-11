3b3c4b1afde7abed93dedf2eef7d55b22164c883
2605736 <2605736@sit.singaporetech.edu.sg>
2026-09-12 06:46:52 +0800

    created a continuous loop prompting for stock quantity until usr types quit

--- Week2_Lab/auditor.py
+++ Week2_Lab/auditor.py
@@ -1,7 +1,18 @@
 def main ():
-    # Initialize variables
+    # initialize variables
     total_inventory = 0
     failed_entries = 0
+
+    print("--- Smart Inventory Auditor ---")
+    print("Type 'quit' to exit:\n")
+
+    # continuous loop
+    while True:
+        usr_input = input("Enter stock quantity (tupe): ")
+
+        # check for exit command 
+        if usr_input.lower() == "quit":
+            break
 
 if __name__ == "__main__":
     main()
